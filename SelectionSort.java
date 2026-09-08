04758f9924b72c152c18a69c054f87b552e2ddf0
Akarsh Srivastava <akarsh.neural@gmail.com>
2026-09-08 11:01:39 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SelectionSort{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<n-1; i++){
+            int min = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[i] < arr[min]){
+                    min=i;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+            }
+            for(int i=0; i<n; i++){
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
